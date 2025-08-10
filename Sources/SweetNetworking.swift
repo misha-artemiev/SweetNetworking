218a750576cfedda27e07228297788eff2b4e637
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2025-08-10 01:01:12 +0100

    added draft of a draft subcommand

--- Sources/SweetNetworking.swift
+++ Sources/SweetNetworking.swift
@@ -9,6 +9,8 @@ struct SweetNetworking: ParsableCommand {
         subcommands: [
             Install.self,
             Uninstall.self,
+            Show.self,
+            Draft.self,
         ],
     )
 }
@@ -30,6 +32,68 @@ extension SweetNetworking {
         static let configuration = CommandConfiguration(
             commandName: "uninstall",
             abstract: "Uninstall from systemd",
+        )
+
+        mutating func run() throws {
+        }
+    }
+}
+
+extension SweetNetworking {
+    struct Show: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            commandName: "show",
+            abstract: "Show current configuration",
+        )
+
+        mutating func run() throws {
+        }
+    }
+}
+
+extension SweetNetworking {
+    struct Draft: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            commandName: "draft",
+            abstract: "Draft configuration",
+            subcommands: [
+                Show.self,
+                Apply.self,
+                Discard.self,
+            ],
+        )
+    }
+}
+
+extension SweetNetworking.Draft {
+    struct Show: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            commandName: "show",
+            abstract: "Show draft configuration",
+        )
+
+        mutating func run() throws {
+        }
+    }
+}
+
+extension SweetNetworking.Draft {
+    struct Apply: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            commandName: "apply",
+            abstract: "Apply draft configuration",
+        )
+
+        mutating func run() throws {
+        }
+    }
+}
+
+extension SweetNetworking.Draft {
+    struct Discard: ParsableCommand {
+        static let configuration = CommandConfiguration(
+            commandName: "discard",
+            abstract: "Discard draft configuration",
         )
 
         mutating func run() throws {
