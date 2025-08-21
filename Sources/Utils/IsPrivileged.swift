b55edf01e8bd4802f7f3ce99743eefde7ee87459
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2025-08-22 00:14:10 +0100

    added privileged validation throw

--- Sources/Utils/IsPrivileged.swift
+++ Sources/Utils/IsPrivileged.swift
@@ -1,4 +1,5 @@
 import Foundation
+import ArgumentParser
 
 struct IsPrivileged {
     static func isPrivilegedEnvironment() -> Bool {
@@ -7,5 +8,12 @@ struct IsPrivileged {
         guard ProcessInfo.processInfo.environment["SUDO_UID"] != nil else { return false }
         guard ProcessInfo.processInfo.environment["SUDO_GID"] != nil else { return false }
         return true       
+    }
+
+    static func validate() throws {
+        guard isPrivilegedEnvironment() else {
+            throw ValidationError("this command must be executed privileged")
+        }
+        return
     }
 }
