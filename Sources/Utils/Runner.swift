c33f617fbaac54ef79f6cfd9c0e7e274f9b7ce66
Mykhailo Artemiev <mykhailo.artemiev@icloud.com>
2025-08-26 01:08:31 +0100

    arguments renamed to argumentsarray in runner

--- Sources/Utils/Runner.swift
+++ Sources/Utils/Runner.swift
@@ -1,10 +1,10 @@
 import Subprocess
 
 struct Runner {
-    static func run(executableName: String, arguments: Array<String> = []) async throws -> CollectedResult<StringOutput<UTF8>, StringOutput<UTF8>> {
+    static func run(executableName: String, argumentsArray: Array<String> = []) async throws -> CollectedResult<StringOutput<UTF8>, StringOutput<UTF8>> {
         let configuration = Subprocess.Configuration(
             executable: .name(executableName),
-            arguments: Arguments.init(arguments),
+            arguments: Arguments.init(argumentsArray),
         )
         return try await Subprocess.run(configuration, output: .string(limit: 1000), error: .string(limit: 1000))
     }
